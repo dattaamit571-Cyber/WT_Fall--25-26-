c5b72797abed87189e69d21aae60897ca24c5419
Amit <dattaamit571@gmail.com>
2025-11-03 18:56:20 +0600

    Lab_Task_1

---
+++ Mid_Term/Lab_task_1.php
@@ -0,0 +1,54 @@
+
+<!DOCTYPE html>
+<html>
+<body>
+    <title>
+    Amit_O
+</title>
+<body>
+    <table>
+    <h1 style="background-color: red; border-bottom: 4px solid black; padding: 10px;">Student Registration From</h1>   
+ 
+Fast Name:
+<input type="text"> <br><br>
+Last Name:
+<input type="textr"> <br><br>
+Student ID:
+<input type="number"> <br><br>
+Program Name:
+<input type="textr"> <br><br>
+Selecet Depertment:
+<input type="text"> <br><br>
+Phon Number:
+<input type="number"> <br><br>
+University Email:
+<input type="text"> <br><br>
+Creat Password:
+<input type="textr"> <br><br>
+Confrom Password:
+<input type="text"> <br><br>
+Semester Selection :
+<input type="radio" name="sms" Value="1st"> Spring_2025
+<input type="radio" name="sms" Value="2nd"> Fall_2025
+<input type="radio" name="sms" Valie="3nd"> Summer_2025
+<input type="radio" name="sms" Valie="4nd"> Other/Special Term <br><br>
+Requsted credit Load(Max):
+<input type="number"> <br><br>
+<input type="radio" name="req" Value="1st"> I requrie academic adversing before final registion <br><br>
+
+<h2 style="background-color: red; border-bottom: 4px solid black; padding: 10px;"> Course Selection:</h2>
+
+<input type="checkbox">MATH
+<input type="checkbox">MICRO
+<input type="checkbox">Accounting
+<input type="checkbox">RM
+<input type="checkbox">SOFTER QUALITY TESTING
+<input type="checkbox">COMPUTER NETWORK
+<input type="checkbox">DATA SCIENC
+<input type="checkbox">DATA MINING <br><br>
+
+Comment/ Special Request:
+
+<input type="text"> <br><br>
+
+<input type="submit" value="submit">
