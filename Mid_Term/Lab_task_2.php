3d3c1dfeda8ce0191c5d4167d9d310677b940227
Amit <dattaamit571@gmail.com>
2025-11-17 11:19:21 +0600

    Work

--- Mid_Term/Lab_task_2.php
+++ Mid_Term/Lab_task_2.php
@@ -1,8 +1,25 @@
 <! DOCTYPE html>
-<html>  
+<html>
 <body>
     <title>WT_Fall (25-26)</title>
 <body> 
     <table>
-    <h1 style="background-color: red; border-bottom: 4px solid black; padding: 10px;">Student Course Drop Form</h1>
-    
+    <h1 style="background-color: red; border-bottom: 4px solid black; padding: 10px;">Perticipant Registration From</h1>
+
+FULL Name:
+<input type="text"> <br><br>
+
+Email:
+<input type="text"> <br><br>
+
+Phone Number:
+<input type="number"> <br><br>
+
+Creat Password:
+<input type="text"> <br><br>
+
+Confrom Password:
+<input type="text"> <br><br>
+<input type="Register" value="Register">
+</body>
+</html>
