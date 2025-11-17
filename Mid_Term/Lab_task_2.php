32604ac17d65231a6e3a494478b1dc7c35682bde
Amit <dattaamit571@gmail.com>
2025-11-17 22:57:21 +0600

    Work

--- Mid_Term/Lab_task_2.php
+++ Mid_Term/Lab_task_2.php
@@ -10,4 +10,13 @@
             <center>
                 <h1>Participents Registration</h1>
             </center>
-            
+Full Name:
+<input type="text" id="name" class="MI" placeholder="Enter your full name"><br><br>   
+Email:
+<input type="text" id="email"class="MI" placeholder="Enter your email"><br><br>
+Phone Number:
+<input type="text" id="phone" class="MI" placeholder="Enter your phone number"><br><br>
+Password:
+<input type="text" id="password" class="MI" placeholder="Enter your password"><br><br>
+Confirm Password:
+<input type="text" id="cpassword" class="MI" placeholder="Confirm your password"><br><br>
