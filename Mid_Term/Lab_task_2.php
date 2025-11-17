9599b49aa5a92e0eddac9cb5737f7266c8d15eed
Amit <dattaamit571@gmail.com>
2025-11-17 22:34:38 +0600

    Work

---
+++ Mid_Term/Lab_task_2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Lab Task 2</title>
+    <Link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <center>
+        <form class="xform" onsubmit="return false">
+            <center>
+                <h1>Participents Registration</h1>
+            </center>
+            
