d1b6258122a3129dc2f04bdbd89e64513d7ccff5
Amit <dattaamit571@gmail.com>
2025-11-18 00:32:38 +0600

    work

--- Mid_Term/Lab_task_2.php
+++ Mid_Term/Lab_task_2.php
@@ -20,3 +20,20 @@ Password:
 <input type="text" id="password" class="MI" placeholder="Enter your password"><br><br>
 Confirm Password:
 <input type="text" id="cpassword" class="MI" placeholder="Confirm your password"><br><br>
+<button id="B1" class="btn" onclick="validateForm()">Register</button>
+<div id="D1" style="background-color: indianred; color: white; border-radius: 10px; border-color: red;margin: 10px 0px;"></div>
+ <div id="D2" style="background-color:  lightgreen; color: white; border-radius: 10px; border-color: green;"></div>
+        </form>
+<form class="xform" onsubmit="return false">
+    <center><h1>Activity Selection</h1></center>
+Activity Name:
+<input type="text" id="aname" class="MI" placeholder="Enter activity name"><br><br>
+<button id="B2" class="MB" onclick="AddActivity()">Add Activity</button>
+            <br> <br>
+<div id="D3" style="background-color: indianred; color: white; border-radius: 10px; border-color: red;margin: 10px 0px;"></div>
+<button id="B3" style="border: solid; border-color: red; background-color: indianred; color: white; margin: 10px 0px" onclick="RemoveActivity()">Delete</button>
+</form>
+    </center>
+    <script src="Mid_Term/Lab_task_Validation.js"></script> 
+</body>
+</html>
