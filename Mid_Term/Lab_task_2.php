cff10bc1ecbc3b75374763e214079194c68291d2
Amit <dattaamit571@gmail.com>
2025-11-17 10:44:50 +0600

    Work

---
+++ Mid_Term/Lab_task_2.php
@@ -0,0 +1,7 @@
+!<DOCTYPE htem>
+<html>
+<body>
+    <title>WT_Fall (25-26)</title>
+<body>
+    <table>
+    <h1 style="background-color: red; border-bottom: 4px solid black; padding: 10px;">Student Registration From</h1>
