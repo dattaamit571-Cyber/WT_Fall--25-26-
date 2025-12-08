dc1686ad75ded256f92050c4fd91a92d628ec88a
Amit <dattaamit571@gmail.com>
2025-12-08 12:17:46 +0600

    Class

--- Final_Term/Classwork.php
+++ Final_Term/Classwork.php
@@ -5,3 +5,15 @@
     "dept"  => "CSE",
     "marks" => array(85, 78, 92, 74)
 );
+echo "<h2>Student Information</h2>";
+foreach ($student as $key => $value) {
+    if ($key == "marks") {
+        echo "Marks: ";
+        foreach ($value as $m) {
+            echo $m . " ";
+        }
+        echo "<br>";
+    } else {
+        echo ucfirst($key) . ": " . $value . "<br>";
+    }
+}
