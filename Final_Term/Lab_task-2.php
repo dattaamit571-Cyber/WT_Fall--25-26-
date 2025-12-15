cc659b115f1fe97a5900d9b117ee3b7a44814c92
Amit <dattaamit571@gmail.com>
2025-12-15 11:52:22 +0600

    class

--- Final_Term/Lab_task-2.php
+++ Final_Term/Lab_task-2.php
@@ -93,3 +93,14 @@ function test_input($data)
         <option value="">Select</option>
         <option value="A+">A+</option>
         <option value="A-">A-</option>
+        <option value="B+">B+</option>
+        <option value="B-">B-</option>
+        <option value="O+">O+</option>
+        <option value="O-">O-</option>
+        <option value="AB+">AB+</option>
+        <option value="AB-">AB-</option>
+    </select>
+    <span style="color:red;"><?php echo $bloodErr; ?></span>
+    <br><br>
+    <input type="submit" value="Submit">
+</form>
