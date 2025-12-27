fe6cc5c9882615bec86334ee236ad86c57307079
Amit <dattaamit571@gmail.com>
2025-12-27 13:34:25 +0600

    classs

--- PROJECT/index1.php
+++ PROJECT/index1.php
@@ -6,3 +6,10 @@ if (isset($_SESSION['user_id'])) {
     exit();
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset+"UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Online Quiz Platform</title>
+    
