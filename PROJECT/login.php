5e95fd955d1ab792381e6544acb703f210840cdd
Amit <dattaamit571@gmail.com>
2025-12-27 21:49:18 +0600

    class

--- PROJECT/login.php
+++ PROJECT/login.php
@@ -15,4 +15,17 @@ session_start();
     
     <link rel="stylesheet" href="css/style.css" />
     <style>
-    
+
+.remember-me {
+    display: flex;
+    align-items: center;
+    gap: 8px; 
+    margin-bottom: 15px;
+    }
+
+.remember-me input[type="checkbox"]{
+    margin: 0;
+    width: 16px;
+    height: 16px;
+    cursor: pointer;
+}
