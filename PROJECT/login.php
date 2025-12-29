3068dfbf6228730834fcc59f5c803186209370ff
Amit <dattaamit571@gmail.com>
2025-12-29 11:50:25 +0600

    class

--- PROJECT/login.php
+++ PROJECT/login.php
@@ -22,7 +22,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST')
 
     if ($email && $password)
 
+ {
+        $sql = "SELECT * FROM users WHERE email = '$email' LIMIT 1";
+        $result = mysqli_query($conn, $sql);
 
+        if ($result && mysqli_num_rows($result) === 1) 
+        {
+            $user = mysqli_fetch_assoc($result);
+            if (password_verify($password, $user['password']))
 
 
 
