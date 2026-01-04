9f9f1d303c97e7b83f494e75cc81b7ae07fa9f2d
Amit <dattaamit571@gmail.com>
2026-01-04 11:40:08 +0600

    class

---
+++ PROJECT/register.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+
+$servername = "localhost";
+$db_username = "root";
+$db_password = "";
+$dbname = "quiz_app"; 
+
+$conn = mysqli_connect($servername, $db_username, $db_password, $dbname);
+
+
+if (!$conn) 
+{
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$successMessage = "";
