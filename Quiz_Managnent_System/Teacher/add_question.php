80d6a339fd9fa7739558c41252e0c82855cb7897
Amit <dattaamit571@gmail.com>
2026-01-17 12:26:51 +0600

    add qs php

---
+++ Quiz_Managnent_System/Teacher/add_question.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'teacher') 
+
+{
+    header("Location: ../dashboard.php");
+    exit();
+}
+$servername = "localhost";
+$db_username = "root";
+$db_password = "";
+$dbname = "quiz_app";
+$conn = mysqli_connect($servername, $db_username, $db_password, $dbname);
+if (!$conn) 
+{
+    die("Connection failed: " . mysqli_connect_error());
+}
