7a6c8a899fb4bb07a5863113d085c0b649beeb9b
Amit <dattaamit571@gmail.com>
2026-01-19 02:06:51 +0600

    delete

--- Quiz_Managnent_System/Teacher/delete_question.php
+++ Quiz_Managnent_System/Teacher/delete_question.php
@@ -13,3 +13,27 @@ $dbname = "quiz_app";
 
 $conn = mysqli_connect($servername, $db_username, $db_password, $dbname);
 if (!$conn)
+{
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$successMessage = "";
+$error = "";
+
+$quiz_id = intval($_GET['quiz_id'] ?? 0);
+$question_id = intval($_GET['question_id'] ?? 0);
+
+if ($question_id && $_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['delete'])) 
+
+{
+    $sql = "DELETE FROM questions WHERE id = $question_id";
+    if (mysqli_query($conn, $sql)) 
+    
+    {
+        $successMessage = "Question deleted successfully.";
+    } 
+    else 
+    {
+        $error = "Failed to delete question.";
+    }
+}
