d34c8cd66dac84e5bc6f7b88eb4205ed2f64bcc7
Amit <dattaamit571@gmail.com>
2026-01-19 02:05:35 +0600

    delete

---
+++ Quiz_Managnent_System/Teacher/delete_question.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'teacher') 
+
+{
+    header("Location: ../dashboard.php");
+    exit();
+}
+$servername = "localhost";
+$db_username = "root";
+$db_password = "";
+$dbname = "quiz_app";
+
+$conn = mysqli_connect($servername, $db_username, $db_password, $dbname);
+if (!$conn)
