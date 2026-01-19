d39eac5fc9fc7efe5f733fe1a7b7b72ad59551f4
Amit <dattaamit571@gmail.com>
2026-01-19 20:29:45 +0600

    dash

--- Quiz_Managnent_System/login.php
+++ Quiz_Managnent_System/login.php
@@ -22,7 +22,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST')
 
     if ($email && $password)
 
- {
+    {
         $sql = "SELECT * FROM users WHERE email = '$email' LIMIT 1";
         $result = mysqli_query($conn, $sql);
 
